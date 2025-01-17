717ee3a0f5db53c98f0cc2b5355071827c851011
Asclepius1 <istileunurik@gmail.com>
2025-01-17 21:46:28 +0500

    fix

--- api/owner.py
+++ api/owner.py
@@ -36,7 +36,7 @@ async def check_all_owner_and_add(session: AsyncSession = Depends(get_async_sess
                 await session.execute(owner.insert(), new_owners_data)
                 await session.commit()
                 return {"message": f"Владелецы были добавлены в кол-ве: {len(new_owners)}"}
-            return {'status_code': 204, 'message': 'Нету новых данных для вставки'}
+            return {'message': 'Нету новых данных для вставки'}
     raise HTTPException(status_code=404, detail="Не получилось добавить владельцев")
 
 

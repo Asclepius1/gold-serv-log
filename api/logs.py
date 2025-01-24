8508ba5231689541724304e18aa2190d795a4ced
Asclepius1 <istileunurik@gmail.com>
2025-01-24 17:32:32 +0500

    test

--- api/logs.py
+++ api/logs.py
@@ -230,6 +230,22 @@ async def delete_errors(
 
     return {"message": f"Удалено {len(error_ids)} ошибок."}
 
+@router.get("/test-test")
+async def test_router(datetime_: str):
+    url = f'{GOLD_SERV_API_URL}'
+    if datetime_:
+        url += f'/?date={datetime_}'
+    else:
+        url += f"/?date={datetime.now().strftime('%d%m%Y')}"
+
+    headers = {'Authorization': f'Bearer {BEARER_TOKEN_GOLD_SERV}'}
+    response = requests.get(url, headers=headers, verify=False)
+    if response.status_code == 200:
+        data: list[dict] = response.json()
+        return data
+    else:
+        return response.text
+    
 @router.post("/add_logs")
 async def add_logs(datetime_: str | None = None, session: AsyncSession = Depends(get_async_session), user: User = Depends(superuser_required)):
     latest_current_log = select(logs).order_by(logs.c['datetime'].desc()).limit(1)
